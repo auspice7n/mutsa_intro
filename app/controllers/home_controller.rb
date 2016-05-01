11b6dda44210c04347cc01b9826aca1e928a4c12
HyunSeo Nam <auspice7@naver.com>
2016-05-01 06:43:47 +0000

    Initialize repository

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def mission
+    end
+    
+    def service
+    end
+    
+    def team
+    end
+    
+    def contact
+    end
+    
+end
